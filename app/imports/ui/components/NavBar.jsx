8bacce9ed7ceea972be98e1c7020a55ce9eafbc3
jhunDomingo <jhunheinrichdomingo@gmail.com>
2019-04-08 12:09:18 -1000

    Navbar v2

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,77 +3,119 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown } from 'semantic-ui-react';
+import { Menu, Header, Popup, Icon, Button, Modal, Form } from 'semantic-ui-react';
 import { Roles } from 'meteor/alanning:roles';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
-  state = { activeItem: 'home' }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  /** Initialize component state with properties for login and redirection. */
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '', error: '', redirectToReferer: false };
+    // Ensure that 'this' is bound to this component in these two functions.
+    // https://medium.freecodecamp.org/react-binding-patterns-5-approaches-for-handling-this-92c651b5af56
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  /** Update the form controls each time the user interacts with them. */
+  handleChange(e, { name, value }) {
+    this.setState({ [name]: value });
+  }
+
+  /** Handle Signin submission using Meteor's account mechanism. */
+  handleSubmit() {
+    const { email, password } = this.state;
+    Meteor.loginWithPassword(email, password, (err) => {
+      if (err) {
+        this.setState({ error: err.reason });
+      } else {
+        this.setState({ error: '', redirectToReferer: true });
+      }
+    });
+  }
+
+  /** Handle Signup submission using Meteor's account mechanism. */
 
   render() {
-    const { activeItem } = this.state
+    const menuStyle = { borderRadius: '0', height: '50px' };
+
+    const popupStyle = {
+      borderRadius: '0',
+      width: '200px',
+      backgroundColor: '#1B1C1D',
+      border: 'none',
+    };
+
+    const titleStyle = {
+      fontSize: '25px',
+    };
 
     return (
         <div>
           {this.props.currentUser === '' ? (
-              <Menu pointing secondary>
-                <Menu.Item
-                    name='home'
-                    active={activeItem === 'home'}
-                    onClick={this.handleItemClick}
-                    as={NavLink}
-                    exact to="/"
-                />
-                <Menu.Item
-                    name='about'
-                    active={activeItem === 'about'}
-                    onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                    name='join'
-                    active={activeItem === 'join'}
-                    onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                    name='contact'
-                    active={activeItem === 'contact'}
-                    onClick={this.handleItemClick}
-                />
+              <Menu style={menuStyle} attached="top" borderless inverted>
+                <Menu.Item style={{ left: '45%' }}>
+                  <Header style={titleStyle} inverted color={'yellow'} as={'a'} href={"/"}>Waggle</Header>
+                </Menu.Item>
                 <Menu.Menu position='right'>
-                  {this.props.currentUser === '' ? (
-                      <Menu.Item
-                          name='Sign in'
-                          as={NavLink}
-                          exact to="/signin"
-                      />
-                  ) : (
-                      <Dropdown item text={this.props.currentUser}>
-                        <Dropdown.Menu>
-                          <Dropdown.Item icon='edit' text='Edit Profile' as={NavLink} exact to='/signout'/>
-                          <Dropdown.Item icon='sign-out' text='Sign Out' as={NavLink} exact to='/signout'/>
-                        </Dropdown.Menu>
-                      </Dropdown>
-                  )}
+                  <Modal trigger={<Menu.Item>Log In</Menu.Item>}>
+                    <Modal.Header>Login to your account</Modal.Header>
+                    <Modal.Content>
+                      <Form onSubmit={this.handleSubmit}>
+                        <Form.Input
+                            label="Email"
+                            icon="user"
+                            iconPosition="left"
+                            name="email"
+                            type="email"
+                            placeholder="E-mail address"
+                            onChange={this.handleChange}
+                        />
+                        <Form.Input
+                            label="Password"
+                            icon="lock"
+                            iconPosition="left"
+                            name="password"
+                            placeholder="Password"
+                            type="password"
+                            onChange={this.handleChange}
+                        />
+                        <Form.Button content="Submit"/>
+                      </Form>
+                    </Modal.Content>
+                  </Modal>
                 </Menu.Menu>
               </Menu>
           ) : (
-              <Menu pointing secondary>
-                <Menu.Item
-                    name='home'
-                    active={activeItem === 'home'}
-                    onClick={this.handleItemClick}
-                    as={NavLink}
-                    exact to="/"
-                />
+              <Menu style={menuStyle} attached="top" borderless inverted>
+                <Menu.Item style={{ left: '45%' }}>
+                  <Header inverted color={'yellow'} as='h1'>Waggle</Header>
+                </Menu.Item>
                 <Menu.Menu position='right'>
-                  <Dropdown item text={this.props.currentUser}>
-                    <Dropdown.Menu>
-                      <Dropdown.Item icon='edit' text='Edit Profile' as={NavLink} exact to='/signout'/>
-                      <Dropdown.Item icon='sign-out' text='Sign Out' as={NavLink} exact to='/signout'/>
-                    </Dropdown.Menu>
-                  </Dropdown>
+                  <Menu.Item>
+                    <Popup basic
+                           on={'click'}
+                           horizontalOffset={15}
+                           verticalOffset={-4}
+                           style={popupStyle}
+                           trigger={
+                             <Button inverted icon size={'mini'} color={'yellow'}>
+                               <Icon name={'bars'}/>
+                             </Button>
+                           }
+                    >
+                      <Menu vertical borderless secondary>
+                        <Menu.Item as={'a'} href={''} style={{ color: 'rgba(255, 255, 255, 0.9)' }}>Create a
+                          Session</Menu.Item>
+                        <Menu.Item as={'a'} href={''} style={{ color: 'rgba(255, 255, 255, 0.9)' }}>View
+                          Profile</Menu.Item>
+                        <Menu.Item as={NavLink} exact to="/signout" style={{ color: 'rgba(255, 255, 255, 0.9)' }}>Log
+                          Out</Menu.Item>
+                      </Menu>
+                    </Popup>
+                  </Menu.Item>
                 </Menu.Menu>
               </Menu>
           )}
@@ -85,6 +127,7 @@ class NavBar extends React.Component {
 /** Declare the types of all properties. */
 NavBar.propTypes = {
   currentUser: PropTypes.string,
+  location: PropTypes.object,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
