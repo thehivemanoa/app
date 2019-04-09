327e561807449e5209c7bedfa913c0072cfa0d91
Conrad Wolfe <46543798+conradwolfe@users.noreply.github.com>
2019-04-08 18:39:56 -1000

    Initial Commit for issue-1
    Add some rough boilerplate for landing page and edited main.html <title>

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,20 +1,43 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header } from 'semantic-ui-react';
+import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <Grid verticalAlign='middle' textAlign='center'>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+          <Grid.Row style={{ backgroundColor: '#081B34', padding: '56px 168px' }}>
+            <Header style={{ color: 'white' }}>Register Account</Header>
+            <Button style={{ backgroundColor: '#FAA952' }}>REGISTER NOW</Button>
+          </Grid.Row>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
+          <Grid.Row columns={2} style={{ backgroundColor: 'white', padding: '56px' }}>
+            <Grid.Column width={5}>
+              <Header>What is Waggle?</Header>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae augue eget augue dignissim
+              porta id vel lorem. Integer et lacus tempor, lacinia magna vehicula, gravida ante. Nulla facilisi.
+              Curabitur semper, elit sed dignissim vehicula, mi velit viverra orci, id tristique erat tellus ac ligula.
+              Praesent vehicula sodales nisl. Duis sed magna nisi. Morbi eleifend a velit ac luctus. Vestibulum ante
+              ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Image src={'https://semantic-ui.com/images/wireframe/image.png'} style={{ width: '500px' }}/>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row style={{ backgroundColor: 'yellow', padding: '56px 168px' }}>
+            <Header>Schedule a session</Header>
+          </Grid.Row>
+
+          <Grid.Row style={{ backgroundColor: 'white', padding: '56px 168px' }}>
+            <Header>Calendar</Header>
+          </Grid.Row>
+
+          <Grid.Row style={{ backgroundColor: 'black', padding: '56px 168px' }}>
+            <Header style={{ color: 'white' }}>Move up the leaderboard</Header>
+          </Grid.Row>
 
         </Grid>
     );
