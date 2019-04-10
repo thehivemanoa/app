35bcf8c162a4ec5687d042d1fc2402ed9632ba4d
MorganStremick <sm60@hawaii.edu>
2019-04-10 01:29:29 -1000

    Updated calendar page routing

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -27,7 +27,7 @@ class App extends React.Component {
               <Route exact path="/" component={Landing}/>
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
-              <ProtectedRoute path="/list" component={CalendarPage}/>
+              <ProtectedRoute path="/calendar" component={CalendarPage}/>
               <ProtectedRoute path="/add" component={SearchPage}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
               <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
