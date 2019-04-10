3773e6c9858cf287f4bc637d3357acce4f16dc11
vgerber614 <43424164+vgerber614@users.noreply.github.com>
2019-04-10 11:28:31 -1000

    Update CourseCard.jsx

--- app/imports/ui/components/CourseCard.jsx
+++ app/imports/ui/components/CourseCard.jsx
@@ -3,18 +3,12 @@ import PropTypes from 'prop-types';
 import { Card, Header, List, Grid, Button, Icon } from 'semantic-ui-react';
 import dateFns from 'date-fns';
 
-export default class SessionCard extends React.Component {
+export default class CourseCard extends React.Component {
   render() {
     const colors = {
       'ICS 311': '#E692F8',
       'ICS 314': '#FFB4B0',
     };
-    const startTime = this.props.session.startTime;
-    const endTime = this.props.session.endTime;
-    const formattedStartTime = `${dateFns.format(startTime, 'h')}:${dateFns.format(startTime, 'mm')}` +
-        ` ${dateFns.format(startTime, 'a')}`;
-    const formattedEndTime = `${dateFns.format(endTime, 'h')}:${dateFns.format(endTime, 'mm')}` +
-        ` ${dateFns.format(endTime, 'a')}`;
     const headerRowStyle = {
       padding: '0px',
       height: '40px',
@@ -56,14 +50,14 @@ export default class SessionCard extends React.Component {
                   <Header as="h5" style={headerStyle}>{this.props.session.title}</Header>
                 </Grid.Column>
                 <Grid.Column width={6} style={headerColumnStyle}>
-                  <Button style={headerButtonStyle}>Join</Button>
+                  <Button style={headerButtonStyle}>Edit</Button>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
             <Grid>
               <Grid.Row centered columns="equal" style={{ paddingTop: '10px', paddingBottom: '10px' }}>
                 <Grid.Column>
-                  <p style={{ textAlign: 'left' }}>{`${formattedStartTime} - ${formattedEndTime}`}</p>
+                  <p> Bee Status </p>
                 </Grid.Column>
                 <Grid.Column width={1}><Icon name="user times"/></Grid.Column>
                 <Grid.Column width={1}>1</Grid.Column>
@@ -77,6 +71,6 @@ export default class SessionCard extends React.Component {
   }
 }
 
-SessionCard.propTypes = {
+CourseCard.propTypes = {
   session: PropTypes.object.isRequired,
 };
