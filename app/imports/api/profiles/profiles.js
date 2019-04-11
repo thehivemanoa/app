9c3b72586931779e207075caec7ecf5cc254eb19
jhunDomingo <jhunheinrichdomingo@gmail.com>
2019-04-10 15:10:39 -1000

    account v1.0

---
+++ app/imports/api/profiles/profiles.js
@@ -0,0 +1,28 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Create a Meteor collection. */
+const Profiles = new Mongo.Collection('Profiles');
+
+/** Create a schema to constrain the structure of documents associated with this collection. */
+const ProfileSchema = new SimpleSchema({
+  firstName: String,
+  lastName: String,
+  courses: Array,
+  'courses.$.title': String,
+  'courses.$.status': String,
+  joinedSessions: Array,
+  'joinedCourses.$.courses': String,
+  createdSessions: Array,
+  'createdCourses.$.courses': String,
+  level: Number,
+  image: String,
+  owner: String
+}, { tracker: Tracker });
+
+/** Attach this schema to the collection. */
+Profiles.attachSchema(ProfileSchema);
+
+/** Make the collection and schema available to other code. */
+export { Profiles, ProfileSchema };
