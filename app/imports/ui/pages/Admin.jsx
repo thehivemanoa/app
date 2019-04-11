a58ae71ed6ed6ea609fa89aadae61c0ecfb00475
Conrad Wolfe <46543798+conradwolfe@users.noreply.github.com>
2019-04-10 22:59:23 -1000

    Boilerplate for Admin and DataCard

--- app/imports/ui/pages/Admin.jsx
+++ app/imports/ui/pages/Admin.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Grid, Header, Card } from 'semantic-ui-react';
+import DataCard from '../components/DataCard';
 
 class Admin extends React.Component {
   render() {
     return (
-        <Container>
-          <Header>Admin Page</Header>
-        </Container>
+        <Grid container>
+          <Grid.Row centered columns={4}>
+            <Grid.Column>
+                <DataCard icon={'coffee'} value={1} text={'Way'}/>
+            </Grid.Column>
+            <Grid.Column>
+                <DataCard icon={'envelope'} value={2} text={'Say'}/>
+            </Grid.Column>
+            <Grid.Column>
+                <DataCard icon={'tag'} value={3} text={'Words'}/>
+            </Grid.Column>
+            <Grid.Column>
+                <DataCard icon={'comment'} value={4} text={'You'}/>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
     );
   }
 }
