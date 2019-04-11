47d8225cb06f462486df0754cde22de6a33728a5
Conrad Wolfe <46543798+conradwolfe@users.noreply.github.com>
2019-04-10 20:30:38 -1000

    Create Admin.jsx

---
+++ app/imports/ui/pages/Admin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Header } from 'semantic-ui-react';
+
+class Admin extends React.Component {
+  render() {
+    return (
+        <Container>
+          <Header>Admin Page</Header>
+        </Container>
+    );
+  }
+}
+
+export default Admin;
