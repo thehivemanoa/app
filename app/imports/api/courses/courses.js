70df61f13feaf37156f67b65e31a6ea4165cd65d
Conrad Wolfe <46543798+conradwolfe@users.noreply.github.com>
2019-04-11 12:54:12 -1000

    Create Course collection and schema
    Course contains a course name and description

---
+++ app/imports/api/courses/courses.js
@@ -0,0 +1,18 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Create a Meteor collection. */
+const Courses = new Mongo.Collection('Courses');
+
+/** Create a schema to constrain the structure of documents associated with this collection. */
+const CourseSchema = new SimpleSchema({
+  course: String,
+  description: String,
+}, { tracker: Tracker });
+
+/** Attach this schema to the collection. */
+Courses.attachSchema(CourseSchema);
+
+/** Make the collection and schema available to other code. */
+export { Courses, CourseSchema };
