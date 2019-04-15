9f1c4b9bb5bf95e6eb6bed29849c5b8a0692442a
vgerber614 <43424164+vgerber614@users.noreply.github.com>
2019-04-14 22:07:40 -1000

    Added mockup profile card.

--- app/imports/ui/pages/UserProfile.jsx
+++ app/imports/ui/pages/UserProfile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Loader, Grid, Divider } from 'semantic-ui-react';
+import { Container, Loader, Grid, Divider, Card, Image, Icon, Progress } from 'semantic-ui-react';
 import { Profiles } from '/imports/api/profiles/profiles';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
@@ -15,18 +15,68 @@ class UserProfile extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
+
+    const containerPadding = {
+      paddingTop: 20,
+      paddingBottom: 30,
+      paddingLeft: 50,
+      paddingRight: 50,
+    }
+
+    const center = {
+      position: 'absolute',
+      left: '.8em',
+      bottom: '.6em',
+      width: '100%',
+      textAlign: 'center',
+      fontSize: '1em',
+    }
+
+    const xpIcon = {
+      fontSize: '3em',
+      width: '100%',
+      height: '0',
+    }
+
     return (
-        <div className="profile-page">
+        <Container className="profile-page" style={containerPadding}>
 
           {/** 2 column grid */}
           <Grid columns={2} divided>
             <Grid.Column>
               { /** *** PROFILE CARD **** */}
-              {/** profile picture (popup edit pfp modal) */}
-              {/** Banner with user name */}
-              {/** Honey pot with level */}
-              {/** current/total XP */}
-              {/** Progress Bar */}
+              <Card>
+                <Card.Content>
+                  <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' circular
+                         style={{ marginBottom: 5 }}/>
+                  <div className="non-semantic-protector">
+                    <h1 className="ribbon">
+                      <strong className="ribbon-content">John Smith</strong>
+                    </h1>
+                    <Grid columns={2} verticalAlign='middle'>
+                      <Grid.Column width={3}>
+                        {/** Honey pot with level */}
+                        <div style={{ position: 'relative' }}>
+                          <Icon name="star outline" style={xpIcon}/>
+                          <div style={center}>
+                            <h2 style={{ fontSize: 18 }}>12</h2>
+                          </div>
+                        </div>
+                      </Grid.Column>
+                        <Grid.Column width={13}>
+                          <Grid.Row>
+                            {/** current/total XP */}
+                            <p>800/1000 XP</p>
+                          </Grid.Row>
+                          <Grid.Row>
+                            {/** Progress Bar*/}
+                            <Progress value='4' total='5' progress='percent'/>
+                          </Grid.Row>
+                        </Grid.Column>
+                    </Grid>
+                  </div>
+                </Card.Content>
+              </Card>
             </Grid.Column>
             <Grid.Column>
               {/** column 2 = 2 rows = My Courses and Account Information */}
@@ -47,34 +97,35 @@ class UserProfile extends React.Component {
               </Grid.Row>
             </Grid.Column>
           </Grid>
-        </div>
-    );
-  }
-}
+          </Container>
+          )
+          ;
+          }
+          }
 
-/** Require an array of Stuff documents in the props. */
-UserProfile.propTypes = {
-  ready: PropTypes.bool.isRequired,
-};
+          /** Require an array of Stuff documents in the props. */
+          UserProfile.propTypes = {
+          ready: PropTypes.bool.isRequired,
+        };
 
-/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-/** export default withTracker(() => {
-  // Get access to Profile documents.
-  const subscription = Meteor.subscribe('Profiles');
-  return {
-    stuffs: Stuffs.find({}).fetch(),
-    ready: subscription.ready(),
-  };
+          /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+          /** export default withTracker(() => {
+            // Get access to Profile documents.
+            const subscription = Meteor.subscribe('Profiles');
+            return {
+              stuffs: Stuffs.find({}).fetch(),
+              ready: subscription.ready(),
+            };
 
-  // Get access to Profile documents.
+            // Get access to Profile documents.
 
-})(UserProfile); */
+          })(UserProfile); */
 
 
-export default withTracker(() => {
-  const subscription = Meteor.subscribe('Profiles');
-  return {
-    profiles: Profiles.find({}).fetch(),
-    ready: subscription.ready(),
-  };
-})(UserProfile);
+          export default withTracker(() => {
+          const subscription = Meteor.subscribe('Profiles');
+          return {
+          profiles: Profiles.find({}).fetch(),
+          ready: subscription.ready(),
+        };
+        })(UserProfile);
