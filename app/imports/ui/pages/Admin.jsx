16689c7a7ea31ffc1be06b54210b7862d6402872
Conrad Wolfe <46543798+conradwolfe@users.noreply.github.com>
2019-04-15 17:29:46 -1000

    In progress admin code
    Kinda broken but changes will be lost when I switch branches so rip

--- app/imports/ui/pages/Admin.jsx
+++ app/imports/ui/pages/Admin.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
 import { Grid, Header, Card } from 'semantic-ui-react';
-import TaskList from '../components/TaskList'
+import TaskList from '../components/TaskList';
 import DataCard from '../components/DataCard';
+import { Courses } from '../../api/courses/courses';
+import { Sessions } from '../../api/session/session';
+import { AddCourse } from '../components/AddCourse';
 
 class Admin extends React.Component {
   render() {
@@ -30,11 +36,27 @@ class Admin extends React.Component {
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
-            <TaskList/>
+            {/** <AddCourse/> */}
           </Grid.Row>
         </Grid>
     );
   }
 }
 
-export default Admin;
+Admin.propTypes = {
+  courses: PropTypes.array.isRequired,
+  sessions: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const courseSub = Meteor.subscribe('Courses');
+  const sessionsSub = Meteor.subscribe('Sessions');
+  return {
+    courses: Courses.find({}).fetch(),
+    sessions: Sessions.find({}).fetch(),
+    ready: (courseSub.ready() && sessionsSub.ready()),
+  };
+})(Admin);
