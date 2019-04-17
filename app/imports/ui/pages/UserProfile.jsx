43bb0713de8e93bb758eaf0988779850f5c9f715
vgerber614 <43424164+vgerber614@users.noreply.github.com>
2019-04-16 19:19:32 -1000

    Imported ProfileCard.jsx component to UserProfile page.

--- app/imports/ui/pages/UserProfile.jsx
+++ app/imports/ui/pages/UserProfile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Loader, Grid, Card, Image, Icon, Progress, Button, List } from 'semantic-ui-react';
+import { Container, Loader, Grid, Card, Button, List } from 'semantic-ui-react'
+import ProfileCard from '../components/ProfileCard';
 import { Profiles } from '/imports/api/profiles/profiles';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
@@ -24,21 +25,6 @@ class UserProfile extends React.Component {
       minHeight: '70vh',
     }
 
-    const center = {
-      position: 'absolute',
-      left: '.8em',
-      bottom: '.6em',
-      width: '100%',
-      textAlign: 'center',
-      fontSize: '1em',
-    }
-
-    const xpIcon = {
-      fontSize: '3em',
-      width: '100%',
-      height: '0',
-    }
-
     const smallButton = {
       float: 'right',
       marginLeft: '2em',
@@ -51,38 +37,8 @@ class UserProfile extends React.Component {
         <Container className="profile-page" style={containerPadding}>
 
           { /** *** PROFILE CARD **** */}
-          <Card style={{ float: 'left', marginRight: '3em' }}>
-            <Card.Content>
-              <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' circular
-                     style={{ marginBottom: 5 }}/>
-              <div className="non-semantic-protector">
-                <h1 className="ribbon">
-                  <strong className="ribbon-content">John Smith</strong>
-                </h1>
-                <Grid columns={2} verticalAlign='middle'>
-                  <Grid.Column width={3}>
-                    {/** Honey pot with level */}
-                    <div style={{ position: 'relative' }}>
-                      <Icon name="star outline" style={xpIcon}/>
-                      <div style={center}>
-                        <h2 style={{ fontSize: 18 }}>12</h2>
-                      </div>
-                    </div>
-                  </Grid.Column>
-                  <Grid.Column width={13}>
-                    <Grid.Row>
-                      {/** current/total XP */}
-                      <p>800/1000 XP</p>
-                    </Grid.Row>
-                    <Grid.Row>
-                      {/** Progress Bar*/}
-                      <Progress value='4' total='5' progress='percent'/>
-                    </Grid.Row>
-                  </Grid.Column>
-                </Grid>
-              </div>
-            </Card.Content>
-          </Card>
+          <ProfileCard/>
+
           <Grid divided='vertically'>
             {/** column 2 = 2 rows = My Courses and Account Information */}
             {/** scroll bar? */}
@@ -118,11 +74,13 @@ class UserProfile extends React.Component {
                 <List.Item>Email: johnsmith@email.com</List.Item>
                 <List.Item>
                   {/** Change Password button -> modal */}
-                  <Button size='small' floated='left' content='Change Password' style={[smallButton, { marginLeft: 0 }]}/>
+                  <Button size='small' floated='left' content='Change Password'
+                          style={[smallButton, { marginLeft: 0 }]}/>
                 </List.Item>
                 <List.Item>
                   {/** Terminate account button + warning icon -> modal */}
-                  <Button floated='left' size='small' content='Terminate Account' style={[smallButton, { marginLeft: 0 }]}/>
+                  <Button floated='left' size='small' content='Terminate Account'
+                          style={[smallButton, { marginLeft: 0 }]}/>
                 </List.Item>
               </List>
             </Grid.Row>
