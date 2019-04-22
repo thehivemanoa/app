248d73877f344744b821a90da06580e3ea00fd45
vgerber614 <43424164+vgerber614@users.noreply.github.com>
2019-04-22 10:57:44 -1000

    Attampted to implement weekly sessions card

---
+++ app/imports/ui/components/UpcomingSessionList.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import dateFns from 'date-fns';
+import PropTypes from 'prop-types';
+import { Card, Header, Divider, Container } from 'semantic-ui-react';
+import SessionCard from './SessionCard';
+import isAfter from 'date-fns/is_after'
+import startOfWeek from 'date-fns/start_of_week'
+import endOfWeek from 'date-fns/end_of_week'
+
+const _ = require('underscore');
+
+export default class UpcomingSessionList extends React.Component {
+  render() {
+    const date = this.props.selectedDate;
+
+    const upcomingSessions = _.sortBy(_.filter(this.props.sessions, (session) => { return isAfter(date, session.startTime) }, 'startTime'));
+
+    const groupedSessions = _.groupBy(upcomingSessions, function (session) {
+      return startOfWeek(session.startTime)
+    });
+    /** groupedSessions returns object grouped by startTime as keys:
+     *  {startTime: [session1, sessions2], startTime: [session3]}
+     */
+
+    const formattedDates = _.map(_.keys(groupedSessions), startDate => {
+      const startDayOfTheWeek = dateFns.format(startDate, 'dddd');
+      const startMonth = dateFns.format(startDate, 'MMM');
+      const startDayOfTheMonth = dateFns.format(startDate, 'DD');
+      const endDate = endOfWeek(startDate);
+      const endDayOfTheWeek = dateFns.format(endDate, 'dddd');
+      const endMonth = dateFns.format(endDate, 'MMM');
+      const endDayOfTheMonth = dateFns.format(endDate, 'DD');
+      return (`${startDayOfTheWeek}, ${startMonth} ${startDayOfTheMonth} - ${endDayOfTheWeek}, ${endMonth} ${endDayOfTheMonth}`);
+    });
+    /** formattedDates array returns new array of keys as formatted start to end dates: ['start-end', 'start-end', 'start-end'] */
+
+    const sessionCards =
+        _.map(_.values(groupedSessions), (sessionsArray) => {
+              _.map(sessionsArray, (session) => {
+                const isJoined = this.props.isJoined(session._id);
+                const handleUpdate = isJoined ? this.props.handleLeave : this.props.handleJoin;
+                return (
+                    <SessionCard
+                        key={session.startTime}
+                        session={session}
+                        fluid
+                        handleUpdate={() => handleUpdate(session._id)}
+                        isJoined={isJoined}
+                    />
+                );
+              })
+            }
+        );
+    /** sessionCards array returns new array of grouped session cards arrays of keys as formatted start to end dates: [ [{sessionCard1}, {sessionCard2}], [{sessionCard3}] ] */
+
+const groupedCards = _.object(formattedDates, sessionCards);
+    /** groupedCards returns new abject where keys are formatted dates and values are arrays of session cards: {'start-end': [{sessionCard1}, {sessionCard2}], 'start-end': {sessionCard3}]} */
+
+
+    /** if there are no upcoming sessions, return "no upcoming sessions" message */
+    if (_.isEmpty(groupedCards)) {
+      return (
+          <Header>
+            You have no upcoming sessions.
+          </Header>
+      );
+    } else {
+      /** return group of cards for each week */
+      return (_.each(groupedCards, (card) => {
+        _.each(_.keys(card), (key) => {
+          return (
+              <Container className="session_weekly_list">
+                <Divider horizontal><h2>{key}</h2></Divider>
+                <Card.Group centered items={_.values(_.pick(card, key))}/>
+              </Container>
+          )
+        })
+      }));
+    }
+  }
+}
+
+UpcomingSessionList.propTypes = {
+  isJoined: PropTypes.func.isRequired,
+  handleJoin: PropTypes.func.isRequired,
+  handleLeave: PropTypes.func.isRequired,
+  sessions: PropTypes.array.isRequired,
+  selectedDate: PropTypes.object.isRequired,
+};
--- app/imports/ui/components/UserHomepage.jsx
+++ app/imports/ui/components/UserHomepage.jsx
@@ -1,14 +1,63 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Grid, Container, Divider, Button, Card, Image, Header, Icon } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
-import { Sessions } from '/imports/api/session/session';
-import SessionCard from '/imports/ui/components/SessionCard';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { Bert } from 'meteor/themeteorchef:bert';
+import { Sessions } from '../../api/session/session';
+import UpcomingSessionList from '/imports/ui/components/UpcomingSessionList';
 
 /** Renders a table containing all of the Session documents. Use <SessionCard> to render each row. */
 class UserHomepage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.currentDate = new Date();
+    this.handleLeave = this.handleLeave.bind(this);
+    this.handleJoin = this.handleJoin.bind(this);
+    this.isJoined = this.isJoined.bind(this);
+  }
+
+  handleLeave(sessionId) {
+    const currentUserId = this.props.currentUserId;
+    const throwError = function (error) {
+      return error ? Bert.alert({ type: 'danger', message: `Leave failed: ${error.message}` }) :
+          Bert.alert({ type: 'success', message: 'Leave succeeded' });
+    };
+    Meteor.users.update(
+        currentUserId,
+        { $pull: { 'profile.joinedSessions': sessionId } },
+        error => throwError(error),
+    );
+    Sessions.update(
+        sessionId,
+        { $pull: { attendees: this.props.currentUsername } },
+        error => throwError(error),
+    );
+  }
+
+  handleJoin(sessionId) {
+    const currentUserId = this.props.currentUserId;
+    const throwError = function (error) {
+      return error ? Bert.alert({ type: 'danger', message: `Join failed: ${error.message}` }) :
+          Bert.alert({ type: 'success', message: 'Join succeeded' });
+    };
+    Meteor.users.update(
+        currentUserId,
+        { $push: { 'profile.joinedSessions': sessionId } },
+        error => throwError(error),
+    );
+    Sessions.update(
+        sessionId,
+        { $push: { attendees: this.props.currentUsername } },
+        error => throwError(error),
+    );
+  }
+
+  isJoined(sessionId) {
+    const joinedSessions = this.props.joinedSessions;
+    return _.some(joinedSessions, session => session === sessionId);
+  }
 
   render() {
 
@@ -31,26 +80,18 @@ class UserHomepage extends React.Component {
           <Grid columns={2} divided>
             <Grid.Column width={12}>
               { /** *** UPCOMING SESSIONS **** */}
-              <Grid columns='equal' verticalAlign='middle'>
-                  <h2> Upcoming Sessions </h2>
-              </Grid>
+                <Grid.Row>
+                <h2> Upcoming Sessions</h2>
+                </Grid.Row>
               <Grid.Row>
-                <Divider horizontal><h2> Monday, April 9 </h2></Divider>
                 {/** Session Cards */}
-                <Card.Group>
-                  <Card>
-                    <Card.Content>
-                      <Card.Header>ICS 314</Card.Header>
-                      <Card.Meta>Worker Bee</Card.Meta>
-                    </Card.Content>
-                  </Card>
-                  <Card>
-                    <Card.Content>
-                      <Card.Header>ICS 311</Card.Header>
-                      <Card.Meta>Royal Bee</Card.Meta>
-                    </Card.Content>
-                  </Card>
-                </Card.Group>
+                <UpcomingSessionList
+                    sessions={this.props.sessions}
+                    selectedDate={this.currentDate}
+                    handleLeave={this.handleLeave}
+                    handleJoin={this.handleJoin}
+                    isJoined={this.isJoined}
+                />
               </Grid.Row>
             </Grid.Column>
             <Grid.Column width={4}>
@@ -73,10 +114,10 @@ class UserHomepage extends React.Component {
                 </Card>
               </Card.Group>
               <br/>
-                <Button size='tiny' style={{ backgroundColor: '#4cba6f', color: 'white' }}>
-                  <Icon style={{ opacity: 1 }} name="plus"/>
-                  Add Courses
-                </Button>
+              <Button size='tiny' style={{ backgroundColor: '#4cba6f', color: 'white' }}>
+                <Icon style={{ opacity: 1 }} name="plus"/>
+                Add Courses
+              </Button>
             </Grid.Column>
           </Grid>
         </Container>
@@ -84,4 +125,22 @@ class UserHomepage extends React.Component {
   }
 }
 
-export default (UserHomepage);
+UserHomepage.propTypes = {
+  joinedSessions: PropTypes.array.isRequired,
+  currentUsername: PropTypes.string.isRequired,
+  currentUserId: PropTypes.string.isRequired,
+  sessions: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe('MySessions');
+  return {
+    currentUserId: Meteor.user() ? Meteor.user()._id : '',
+    currentUsername: Meteor.user() ? Meteor.user().username : '',
+    joinedSessions: Meteor.user() ? Meteor.user().profile.joinedSessions : [],
+    sessions: Sessions.find({}).fetch(),
+    ready: (subscription.ready()),
+  };
+})(UserHomepage);
