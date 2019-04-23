88b6428dee170a6156efb2471e6fab7f9e4fa64c
vgerber614 <43424164+vgerber614@users.noreply.github.com>
2019-04-22 22:51:01 -1000

    Finish account.js changes

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -5,43 +5,64 @@ import { AccountInfo } from '../../api/accountInfo/accountInfo.js';
 
 /* eslint-disable no-console */
 
-function createUser(firstName, lastName, email, password, role) {
+function createUser(email, password, firstName, lastName, role) {
   console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
-    firstName: firstName,
-    lastName: lastName,
     username: email,
     email: email,
     password: password,
+    firstName: firstName,
+    lastName: lastName,
   });
   if (role === 'admin') {
     Roles.addUsersToRoles(userID, 'admin');
   }
 }
 
+/** use Meteor.call to call defined methods */
+Meteor.methods({
+  serverCreateUser: function (email, password, firstName, lastName, role) {
+    console.log(`  Creating user ${email}.`);
+    const userID = Accounts.createUser({
+      username: email,
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+    });
+    if (role === 'admin') {
+      Roles.addUsersToRoles(userID, 'admin');
+    }
+    /** set default items */
+    const image = 'https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif?w=640';
+    const level = 1;
+    const exp = 0;
+    const courses = [];
+    const joinedSessions = [];
+    const createdSessions = [];
+    AccountInfo.insert({
+      username: email,
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+      image: image,
+      level: level,
+      exp: exp,
+      courses: courses,
+      joinedSessions: joinedSessions,
+      createdSessions: createdSessions,
+    });
+    Meteor.publish(userID);
+  },
+});
+
 /** When running app for first time, pass a settings file to set up a default user account. */
-if ((Meteor.users.find().count() === 0) && (AccountInfo.find().count() === 0)) {
-  if (Meteor.settings.defaultAccounts && Meteor.settings.defaultAccountInfo) {
+if ((Meteor.users.find().count() === 0)) {
+  if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, role }) => createUser(email, password, role));
+    Meteor.settings.defaultAccounts.map(({ email, password, firstName, lastName, role }) => createUser(email, password, firstName, lastName, role));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
 }
-
-/** Initialize the collection if empty. */
-if (AccountInfo.find().count() === 0) {
-  if (Meteor.settings.defaultAccountInfo) {
-    console.log('Creating default account info.');
-    Meteor.settings.defaultAccountInfo.map(meteorData => Meteor.settings.defaultAccountInfoaddData(meteorData));
-  }
-}
-
-/** This subscription publishes only the documents associated with the logged in user */
-Meteor.publish('AccountInfo', function publish() {
-  if (this.userId) {
-    const id = Meteor.users.findOne(this.userId);
-    return AccountInfo.find({ owner: id });
-  }
-  return this.ready();
-});
