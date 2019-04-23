7b90f526013f8d260c98720b8cc40f71237c1e4d
Conrad Wolfe <46543798+conradwolfe@users.noreply.github.com>
2019-04-22 23:42:16 -1000

    .

--- app/imports/startup/server/accountInfo.js
+++ app/imports/startup/server/accountInfo.js
@@ -1,19 +1,33 @@
 import { Meteor } from 'meteor/meteor';
 import { AccountInfo } from '../../api/accountinfo/accountinfo.js';
 
-/** This subscription publishes all documents regardless of user */
-Meteor.publish('AccountInfo', function publish() {
-  if (this.userId) {
-    return AccountInfo.find({});
+/** Initialize the database with a default data document. */
+function addData(data) {
+  console.log(`  Adding: ${data.name} (${data.owner})`);
+  AccountInfo.insert(data);
+}
+
+/** Initialize the collection if empty. */
+if (AccountInfo.find().count() === 0) {
+  if (Meteor.settings.defaultAccounts) {
+    console.log('Creating default Students.');
+    Meteor.settings.defaultAccounts.map(data => addData(data));
   }
-  return this.ready();
-});
+}
 
 /** This subscription publishes all documents owned by a specific account. */
 Meteor.publish('AccountProfileInfo', function publish() {
   const username = Meteor.users.findOne(this.userId).username;
   if (this.userId) {
     return AccountInfo.find({ owner: username });
+  }
+  return this.ready();
+});
+
+/** This subscription publishes all documents regardless of user */
+Meteor.publish('AccountInfo', function publish() {
+  if (this.userId) {
+    return AccountInfo.find({});
   }
   return this.ready();
 });
