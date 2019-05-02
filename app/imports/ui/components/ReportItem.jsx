52a463b3ac6949bd6b95af756eca73f163825c7a
Conrad Wolfe <46543798+conradwolfe@users.noreply.github.com>
2019-05-01 21:39:41 -1000

    more admin

--- app/imports/ui/components/ReportItem.jsx
+++ app/imports/ui/components/ReportItem.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Feed, Image } from 'semantic-ui-react';
+import { Feed, Image, Button, Label, Icon, Popup } from 'semantic-ui-react';
 
 /** Renders the Page for adding a document. */
 class ReportItem extends React.Component {
 
+  handleClick() {
+    return (null);
+  }
+
   render() {
+    const reason = `Reason: ${this.props.reportItem.description}`;
     return (
         <Feed.Event>
           <Feed.Label>
@@ -14,11 +19,30 @@ class ReportItem extends React.Component {
                    src={'https://philipmjohnson.github.io/images/philip2.jpeg'}/>
           </Feed.Label>
           <Feed.Content>
-            <Feed.Date content={this.props.reportItem.createdAt.toLocaleDateString('en-US')} />
+            <Feed.Date content={this.props.reportItem.createdAt.toLocaleDateString('en-US')}/>
             <Feed.Summary>
-              {this.props.reportItem.description}
+              <Feed.User>{this.props.reportItem.owner}</Feed.User>
+              {' '}reported{' '}
+              <Feed.User>{this.props.reportItem.target}</Feed.User><br/>
+              {reason}
             </Feed.Summary>
           </Feed.Content>
+          <Popup basic
+                 className="collapsable"
+                 on={'click'}
+                 hideOnScroll
+                 trigger={
+                   <Button animated size={'mini'} style={{ height: '30px' }}>
+                     <Button.Content visible>Resolve</Button.Content>
+                     <Button.Content hidden>
+                       <Icon name='arrow right' />
+                     </Button.Content>
+                   </Button>
+                 }
+          >
+            <Button content={'Disregard'}/>
+            <Button content={'Ban'}/>
+          </Popup>
         </Feed.Event>
     );
   }
